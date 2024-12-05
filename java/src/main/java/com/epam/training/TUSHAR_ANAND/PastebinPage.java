5110309345075be947c4fe9705391df97953ac39
Tushar Anand <tusharanand0000@gmail.com>
2024-12-05 23:38:13 +0530

    try

---
+++ java/src/main/java/com/epam/training/TUSHAR_ANAND/PastebinPage.java
@@ -0,0 +1,47 @@
+package com.epam.training.TUSHAR_ANAND;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.time.Duration;
+
+public class PastebinPage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    // Locators
+    private By codeField = By.xpath("//*[@id=\"postform-text\"]");
+    private By expirationDropdown = By.id("select2-postform-expiration-container");
+    private By tenMinutesOption = By.xpath("//li[text()='10 Minutes']");
+    private By titleField = By.xpath("//*[@id=\"postform-name\"]");
+    private By submitButton = By.xpath("//*[@id=\"w0\"]/div[5]/div[1]/div[10]/button");
+
+    public PastebinPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public void enterCode(String code) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(codeField)).sendKeys(code);
+    }
+
+    public void selectExpiration() {
+        wait.until(ExpectedConditions.elementToBeClickable(expirationDropdown)).click();
+        wait.until(ExpectedConditions.elementToBeClickable(tenMinutesOption)).click();
+    }
+
+    public void enterTitle(String title) {
+        driver.findElement(titleField).sendKeys(title);
+    }
+
+    public void submitPaste() {
+        driver.findElement(submitButton).click();
+    }
+
+    public String getCurrentUrl() {
+        return driver.getCurrentUrl();
+    }
+}
